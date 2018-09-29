9329b84f965e07aa09562128d2e759e13cfc5276
WEIRAN YE <wye12@ucsc.edu>
2018-09-28 22:17:23 -0700

    FixSpellingError

--- GAME-230-Hello-World/main.cpp
+++ GAME-230-Hello-World/main.cpp
@@ -3,7 +3,7 @@
 int main() {
 	std::cout << "Hello World!" << std::endl
 		<< "This is my FIRST program in UCSC GPM project." << std::endl
-		<< "Hope I am able to create great games in the next one and a halh years." << std::endl;
+		<< "Hope I am able to create great games in the next one and a half years." << std::endl;
 
 	system("pause");
 
