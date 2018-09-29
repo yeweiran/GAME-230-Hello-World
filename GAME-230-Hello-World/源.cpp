7b018da80dfb8b0cb56f7c05356da8314a4f1946
WEIRAN YE <wye12@ucsc.edu>
2018-09-28 22:12:42 -0700

    HelloWorld

---
+++ GAME-230-Hello-World/源.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+int main() {
+	std::cout << "Hello World!" << std::endl
+		<< "This is my FIRST program in UCSC GPM project." << std::endl
+		<< "Hope I am able to create great games in the next one and a halh years." << std::endl;
+
+	system("pause");
+
+	return 0;
+}
